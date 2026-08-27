59d58d1951dfeb7838da4f1cc5e4fbf8dfa0f660
eCommerce proyectos <equantumg@gmail.com>
2026-08-27 08:58:15 -0300

    fix: redirigir /campamento a /inicio

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/campamento",
+        destination: "/inicio",
+        permanent: true,
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
